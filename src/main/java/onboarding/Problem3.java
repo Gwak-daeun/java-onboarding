8b44cb98415ac6877133679cfccae763a93ebc8b
Gwak-daeun <bbibibbibi22@gmail.com>
2023-07-27 11:57:48 +0900

    feat(Problem3): 369게임 숫자 제시 시, 몇 번 박수를 쳐야 하는지 구하는 기능 구현(메소드)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,12 +8,22 @@ public class Problem3 {
 
         List<Integer> array = new ArrayList<>();
 
+        int clap = 0; // 손뼉 횟수 초기화
+
         for (int i = 1; i <= number; i++) { //1부터 제시한 숫자 까지의 List 만들기
             array.add(i);
+            String str = array.get(i - 1).toString();
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '3' || str.charAt(j) == '6' || str.charAt(j) == '9') {
+                    clap++;
+                    System.out.println("박수 : " + str);
+                }
+            }
         }
 
-        System.out.println(array);
 
+        System.out.println(clap);
 
         int answer = 0;
         return answer;
