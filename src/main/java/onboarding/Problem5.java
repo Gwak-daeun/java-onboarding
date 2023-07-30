15f593585ae162fefc73e9de3e39560193eedd78
Gwak-daeun <bbibibbibi22@gmail.com>
2023-07-30 21:59:53 +0900

    refactor(Problem5): if문과 배열로 리팩터 진행

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -7,82 +7,41 @@ import java.util.List;
 public class Problem5 {
     public static List<Integer> solution(int money) {
 
-        List<Integer> answer = new ArrayList<>(9);
+        List<Integer> answer = new ArrayList<>();
 
-        int remaining; // 계산 후 남은 돈
+        int remaining = money; // 계산 후 남은 돈
 
-        for (int i = 0; i < answer.size(); i++) {
-            
+        int num = 50000;
+
+        int[] arr = new int[]{2, 5, 10};
+
+        for (int i = 0; i < 9; i++) { // 5, 2 나누기 반복. 마지막은 나누기 10
+
+            if (i == 0){ // 0이면
+                answer.add(remaining / 50000);
+                remaining = remaining % 50000; // remaining : 237
+            }
+            if (i == 8) { // 마지막 1원 단위 계산
+                answer.add(remaining / (num / arr[2]));
+            }
+            if (i != 0 && i % 2 == 1) { // 홀수면
+
+                answer.add(remaining / (num / arr[i % 2])); // 0
+                remaining = remaining % (num / arr[i % 2]); //
+                num = num / arr[i % 2];
+            }
+            if (i != 0 && i % 2 == 0 && i != 8){ // 짝수면, 마지막 1의 자리가 아닐 때
+                answer.add(remaining / (num / arr[i % 2]));
+                remaining = remaining % (num / arr[i % 2]);
+                num = num / arr[i % 2];
+            }
         }
 
-        if (money / 50000 != 0) { // 5만으로 나눴을 때 몫이 있다면
-            answer.add(money / 50000);
-            remaining = money % 50000;
-        } else { // 5만으로 나눴을 때 몫이 없다면
-            answer.add(0);
-            remaining = money;
-        }
-        if (remaining / 10000 != 0) { // 1만으로 나눴을 때 몫이 있다면
-            answer.add(remaining / 10000);
-            remaining = remaining % 10000;
-        } else {
-            answer.add(0);
-            remaining = remaining;
-        }
-        if (remaining / 5000 != 0) {
-            answer.add(remaining / 5000);
-            remaining = remaining % 5000;
-        } else {
-            answer.add(0);
-            remaining = remaining;
-        }
-        if (remaining / 1000 != 0) {
-            answer.add(remaining / 1000);
-            remaining = remaining % 1000;
-        } else {
-            answer.add(0);
-            remaining = remaining;
-        }
-        if (remaining / 500 != 0) {
-            answer.add(remaining / 500);
-            remaining = remaining % 500;
-        } else {
-            answer.add(0);
-            remaining = remaining;
-        }
-        if (remaining / 100 != 0) {
-            answer.add(remaining / 100);
-            remaining = remaining % 100;
-        } else {
-            answer.add(0);
-            remaining = remaining;
-        }
-        if (remaining / 50 != 0) {
-            answer.add(remaining / 50);
-            remaining = remaining % 50;
-        } else {
-            answer.add(0);
-            remaining = remaining;
-        }
-        if (remaining / 10 != 0) {
-            answer.add(remaining / 10);
-            remaining = remaining % 10;
-        } else {
-            answer.add(0);
-            remaining = remaining;
-        }
-        if (remaining / 1 != 0) {
-            answer.add(remaining / 1);
-            remaining = remaining % 1;
-        } else {
-            answer.add(0);
-            remaining = remaining;
-        }
         System.out.println(answer);
         return answer;
     }
 
     public static void main(String[] args) {
-        solution(15000);
+        solution(50237);
     }
 }
